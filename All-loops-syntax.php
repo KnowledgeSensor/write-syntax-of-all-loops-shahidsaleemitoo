3e5ac3f34a2edb8a72b635e3d691999215b535fd
shahidsaleemitoo <shahidsaleemitoo@gmail.com>
2025-05-18 09:39:50 -0700

    ready

---
+++ All-loops-syntax.php
@@ -0,0 +1,21 @@
+<?php
+echo "<h1>Syntax of- While Loop</h1><br>";
+While(condition){
+//code to be executed
+ };
+echo "Syntax of- do while loop<br>";
+do{
+    //code to be executed
+} while(condition);
+echo "Syntax of- for loop<br>";
+for(initialization;condition;increment){
+    //code to be executed
+}
+echo "Syntax of- foreach loop<br>";
+foreach($array as $value){
+    //code to be executed
+}
+echo "Or with keys"."<br>";
+foreach($array as $key => $value){
+    //code to be executed
+}
